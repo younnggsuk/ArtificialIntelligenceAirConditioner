1beaa89f6e29a61801131f27715ef36bec1e4f8d
younnggsuk <younnggsuk@gmail.com>
2020-03-06 11:00:29 +0900

    Add some comments

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 
 from my_utils import get_all_face_box, find_face_box_to_track, track_face_box, get_command, visualize_box
 
-
 def load_model(model_path):
     model = tf.saved_model.load(model_path)
     model = model.signatures['serving_default']
@@ -20,15 +19,12 @@ face_detector = load_model(detection_model_path)
 classification_model_path = './models/hand_sign.h5'
 hand_sign_classifier = tf.keras.models.load_model(classification_model_path)
 
-
 # Frame's Width, Height
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
 # Initialize webcam feed
 video = cv2.VideoCapture(0)
-
-
 if not video.isOpened():
     print("Cannot open video")
     exit()
@@ -49,18 +45,18 @@ while(True):
         print("Can't receive frame")
         break
     
-    # Face Detection
+    # Detect all faces
     face_boxes = get_all_face_box(face_detector, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 10)
     
     if len(face_boxes) != 0:
-        # Find 5 sign hand and track that face
+        # Find face with hand sign 5 in the right area
         target_face_box = find_face_box_to_track(hand_sign_classifier, frame, face_boxes)
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) == 27:
             break 
         
         if target_face_box is not None:
-            # Get command sign for 3 seconds
+            # Start command mode for 3 seconds
             start_wait = time.time()
             commands = []
             while(time.time() - start_wait < 3):
@@ -68,21 +64,22 @@ while(True):
                 if cv2.waitKey(1) == 27:
                     exit_flag = True
                     break
-                    
+                
                 ret, frame = video.read()
                 if ret is False:
                     print("Can't receive frame")
                     exit_flag = True
                     break
                 
+                # Face tracking
                 face_boxes = get_all_face_box(face_detector, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 10)
                 if len(face_boxes) == 0:
-                    continue
-                    
+                    continue    
                 tracked_face_box = track_face_box(target_face_box, face_boxes)
                 if tracked_face_box is None:
                     continue
 
+                # Get command sign
                 command_result = get_command(hand_sign_classifier, frame, tracked_face_box)
                 if command_result is not None:
                     hand_area = command_result[0]
@@ -94,7 +91,7 @@ while(True):
                         commands.append(class_idx)
                         visualize_box(frame, hand_area, hand_sign_classes[class_idx], 'Green')
             
-            # Print command result
+            # Print command
             if len(commands) != 0:
                 print(max(set(commands), key = commands.count))
                 print(commands)
